df6ab9f00e7755b3c8177ed627bc67db4de01a7b
13020180033 <61999349+13020180033@users.noreply.github.com>
2020-03-16 22:13:08 +0800

    Add files via upload

---
+++ tugas 1/biodata.java
@@ -0,0 +1,30 @@
+/*
+	nama : yeyen arisanti
+	tgl : 16 maret 2020
+	waktu : 22.30
+*/
+
+
+
+public class biodata {
+	public static void main (String [] args){
+
+	String Nama = "yeyen arisanti";
+	String Nim = "13020180033";
+	String jurusan = "teknik informatika";
+	String fakultas = " ilmu komputer";
+	
+
+	System.out.println("Nama = "+Nama);
+	System.out.println("Nim   =" +Nim);
+	System.out.println("Jurusan  ="+jurusan);
+	System.out.println("Fakultas ="+fakultas);
+	
+
+
+}
+
+
+}
+
+	
